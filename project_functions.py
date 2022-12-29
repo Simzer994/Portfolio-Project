57ff3a54e78cfe43ac39c4f373241c37e2b24961
Simzer994 <simonevanno9@gmail.com>
2022-12-29 20:05:27 +0100

    update

--- project_functions.py
+++ project_functions.py
@@ -111,7 +111,7 @@ def opt_mean_variance(rends:pd.DataFrame,cov:pd.DataFrame,obj_rend:float,risk_fr
 
     # Définition des contraintes
     weights_sum_to_1 = {'type': 'eq',
-                        'fun': lambda weights: np.sum(np.abs(weights)) - 1}
+                        'fun': lambda weights: np.sum(weights) - 1}
 
     if risk_free == None:
         return_is_target = {'type': 'eq',
